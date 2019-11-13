a5c9a970dd5475a897006df82fee271db40f806d
Administrator <Administrator@PC-201708221731>
2019-11-13 16:25:54 +0800

    添加项目文件。

---
+++ _1_2.py
@@ -0,0 +1,28 @@
+words = ["hello","world","span","eggs"]
+counter = 0
+max_index = len(words)-1
+
+while (counter <= max_index):
+    word = words[counter]
+    print(word + "!")
+    counter = counter + 1
+
+print("\n")
+
+for word in words:
+    print(word + "!")
+
+print("\n")
+
+letters = ['a','b','c']
+for m in letters:
+    print(m)
+
+print('\n')
+
+for k in range(5):
+    print("Hello")
+print("\n")
+
+for i in range(0,20,2):
+    print(k)
